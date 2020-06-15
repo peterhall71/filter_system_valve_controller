69a30ff8b22a5f16ca732413dc6210843d716345
peterhall71 <56561695+peterhall71@users.noreply.github.com>
2020-06-15 15:17:57 -0600

    Adding current sensor
    Added initial setup of current sensor and updated README.md

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # filter_system_valve_controller
-Valve controller for well water spin down filter system
 
-#### Hardware
-Adafruit METRO 328  
-Onset AC Current Transformer CTV-A
+Valve controller for well water spin down filter system based on Adafruit METRO.
+
+### Hardware
+
+Adafruit METRO 328 https://github.com/adafruit/Adafruit-METRO-328-PCB  
+Onset AC Current Transformer CTV-A https://www.onsetcomp.com/products/sensors/ctv-x
+Irritrol 700B-.75 https://www.irritrol.com/en/valves/700-series
+
+### File Structure
+
+#### ```/documentation``` folder:  
+A folder holding specifications and documentation on the device hardware.
+
+#### ```filter_system_valve.ino``` file:
+This file is the firmware that will run as the primary application on the METRO. It contains a `setup()` and `loop()` function written in C++.
--- filter_system_valve.ino
+++ filter_system_valve.ino
@@ -1,11 +1,17 @@
 
+
 // photoresistor: https://www.adafruit.com/product/161
+int sensorPinPhoto = A0;    // select the input pin for the photoresistor
+int sensorValuePhoto = 0;   // variable to store the value coming from the sensor
+int limitValuePhoto = 600;
 
+// red led
+// select the pin for the LED
+int ledPin = 2;
 
-int sensorPin = A0;    // select the input pin for the photoresistor
-int ledPin = 2;      // select the pin for the LED
-int sensorValue = 0;  // variable to store the value coming from the sensor
-int limitValue = 500;
+// current sensor: Onset CTV-A 20Amp
+int sensorPinCurrent = A1;  // select the input pin for the current sensor
+int sensorValueCurrent = 0;   // variable to store the value coming from the current sensor
 
 
 void setup() {
@@ -20,23 +26,26 @@ void setup() {
 
 void loop() {
 
-  // read the value from the sensor:
-  sensorValue = analogRead(sensorPin);
+  // read the value from the sensors:
+  sensorValuePhoto = analogRead(sensorPinPhoto);
+  sensorValueCurrent = analogRead(sensorPinCurrent);
 
-  // print reading to serial
-  Serial.print(sensorValue);
-  Serial.println();
+  // print readings to serial
+  Serial.print("Photoresistor: ");
+  Serial.println(sensorValuePhoto);
+  Serial.print("Current Sensor: ");
+  Serial.println(sensorValueCurrent);
 
-  if (sensorValue < limitValue) {
+  if (sensorValuePhoto < limitValuePhoto) {
 
     // turn the LED on (HIGH is the voltage level), wait for one second
     digitalWrite(ledPin, HIGH);
-    delay(5000);
-
-    // turn the LED off by making the voltage LOW, wait for one second
-    digitalWrite(ledPin, LOW);
-
+    delay(1000);
+    
   }
+  
+  // turn the LED off by making the voltage LOW, wait for one second
+  digitalWrite(ledPin, LOW);
 
   delay(1000);
 
